be4cb2e53e7b7eb04c170fba96e762c6d1e842ee
DucThanh1997 <45547213+DucThanh1997@users.noreply.github.com>
2019-01-09 00:24:50 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,63 +1,33 @@
-echo "Welcome to script"
-check_distro()
-{
-os=$(lsb_release -is)   #in ra hệ điều hành
-release=$(lsb_release -rs)   # in ra phiên bản
-if [ $os != 'Ubuntu' ] && [ $release != '16.04' ]
+#!/bin/bash
+echo "CHAO MUNG BAN DEN VOI SCRIPT CUA THANH DEP TRAI"
+echo "Moi ban nhap ip"
+read
+ip="$REPLY"
+echo "Moi ban nhap user"
+read
+user="$REPLY"
+PRI_KEY=~/.ssh/$ip.key
+PUB_KEY=$PRI_KEY.pub
+if [ -f $PRI_KEY ]
 then
-echo "Script available to Ubuntu 16.04!"
-echo "ERROR: This OS is not compatible" >> /var/log/ssh_gen_key.log
-exit 1
-fi
-}
-ssh_gen_key(){
-echo "Enter IP server: "
-read ip_server
-key_pri=~/.ssh/$ip_server.key
-key_pub=$key_pri.pub
-user=$(logname)
-if [ -f $key_pri ]    # Kiểm tra xem đã tồn tại cái này chưa
-then
-echo "Existed key for server."
-echo "Use ssh -o 'StrictHostKeyChecking no' -i $key_pri your_username@$ip_server to login server."
+echo "Da co key ban tu dang nhap di :) "
+echo "Dung cai lenh nay ssh -o 'StrictHostKeyChecking no' -i $PRI_KEY $user@$ip de login nha."
 exit 0
 else
-echo "Enter username to login: "
-read username
-echo "Enter password for user $username: "
-read password
-echo "Generating key pair..."
-ssh-keygen -t rsa -N "" -f $key_pri -q    # tạo key loại rsa vào thư mục của biến key_pri rồi chuyển sang function add_key 
-add_key
-fi
-}
-add_key()
-{
-echo "Adding key on server..."
-install_sshpass # cho chạy fuction install sshpass
-sudo sshpass -p "$password" ssh-copy-id -o "StrictHostKeyChecking no" -i $key_pub -f $username@$ip_server # copy key sang cái server kia
+echo "Ahihi, moi ban nhap password"
+read
+password="$REPLY"
+echo "Cung tai sshpass nao =)))"
+sudo apt-get install sshpass 
+echo "Tao key nha"
+ssh-keygen -t rsa -N "" -f $PRI_KEY 
+sudo sshpass -p "$password" ssh-copy-id -o "StrictHostKeyChecking no" -i $PUB_KEY -f $user@$ip
 if [ $? -eq 0 ]; then
-sudo rm $key_pub
-echo "Use ssh -o 'StrictHostKeyChecking no' -i $key_pri $username@$ip_server to login server."
-echo "Finished."
+sudo rm $PUB_KEY
+echo "Dung cai lenh nay ssh -o 'StrictHostKeyChecking no' -i $PRI_KEY $user@$ip de login nha."
+echo "Hihi."
 else
-sudo rm $key_pri*
-echo "Don't copy key on server"
+sudo rm $PRI_KEY*
+echo "chua duoc thu lai di co loi r"
 fi
-}
-install_sshpass()
-{
-sshpass -V &> /dev/null
-if [ $? -ne 0 ]
-then
-echo "Installing sshpass ..."
-# echo $your_password | sudo -S apt-get install sshpass
-sudo apt-get install sshpass &> /dev/nul
 fi
-}
-main()
-{
-check_distro
-ssh_gen_key
-}
-main
